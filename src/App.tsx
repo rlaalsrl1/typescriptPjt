8f15148cf42019febca0d1f3731b6c8d663c6f7b
rlaalsrl1 <yoyo986756@gmail.com>
2024-09-26 11:34:01 +0900

    tipescript0926

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: black;
+  width: 100vm;
+  height: 100vh;
+`;
+
+function App() {
+  return (
+    <Container className="App">
+      
+    </Container>
+  );
+}
+
+export default App;
